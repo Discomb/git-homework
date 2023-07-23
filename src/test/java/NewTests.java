06387bb826a754a477878d9982eef563aa7daaed
Discomb <lllkoutalll@gmail.com>
2023-07-23 13:13:05 +0300

    added empty test

---
+++ src/test/java/NewTests.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class NewTests {
+    @Test
+    void newSuccessfulSearchTest() {
+
+    }
+}
