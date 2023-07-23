88d04b8f988c1f91a751bc174d1b1c316319e884
Nikolai Malygin <n.p.malygin@gmail.com>
2023-07-23 19:42:51 +0300

    added todo

--- src/test/java/NewTests.java
+++ src/test/java/NewTests.java
@@ -1,12 +1,8 @@
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
-
 public class NewTests {
     @Test
     void newSuccessfulSearchTest() {
-
+        //todo something
     }
 }
