20af6df4976dd71a307361ac56599341053cbaf7
Nikolai Malygin <n.p.malygin@gmail.com>
2023-07-23 19:39:14 +0300

    added link

--- src/test/java/NewTests.java
+++ src/test/java/NewTests.java
@@ -1,12 +1,10 @@
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class NewTests {
     @Test
     void newSuccessfulSearchTest() {
-
+        open("https://www.yandex.ru/");
     }
 }
